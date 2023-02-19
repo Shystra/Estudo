0318e256eb9dfd7144eb92e1bc33fbe1b203517a
Shystra <jlsp.2019@hotmail.com>
2023-02-19 17:49:27 -0300

    Usando Lista

--- Exercicios.py
+++ Exercicios.py
@@ -1,21 +1,61 @@
-pessoa = {
-    'nome': 'luiz',
-    'sobrenome': 'mirante',
-    'idade': 20,
+perguntas = [
+    {
+    'Pergunta': 'Quanto é 2+2? ',
+    'Opções': ['1', '3', '4', '5'],
+    'Resposta': '4',
+    },
+    {
+    'Pergunta': 'Quanto é 5x5? ',
+    'Opções': ['25', '55', '10', '51'],
+    'Resposta': '25',
+    },
+    {
+    'Pergunta': 'Quanto é 10/2? ',
+    'Opções': ['4', '5', '2', '1'],
+    'Resposta': '5',
+    },
+]
 
- 
-    'endereço': [
+qtd_acertos = 0
+for pergunta in perguntas:
+    print ('Pergunta:', pergunta ['Pergunta'])
+    print ()
 
-    {'rua': 'tal tal', 'numero': 123}
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print (f'{i})', opcao)
+    print ()
+
+    escolha = input('Escolha uma Opçõa: ')
+
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+
+    if escolha.isdigit():
+         escolha_int = int(escolha)
+        
+
+    if escolha_int is not None:
+       if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta ['Resposta']:
+                    acertou = True
+
+    print()
+
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou 😊')
+
+    else:
+        print('Errou ✖️')
+
+    print()
+
+print('Você acertou', qtd_acertos)
+print('de', len(perguntas), 'perguntas.')
+
+
+
+        
     
-    ]
-}
-
-print(pessoa['nome'])
-
-print()
-
-#podemos usar o for para trazer a chave inteira
-for chave in pessoa:
-    print(chave, pessoa[chave])
-#0,
