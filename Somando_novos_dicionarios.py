3685e11abb924f7d5d6117d3d9650d87ac8009ec
Shystra <jlsp.2019@hotmail.com>
2023-02-21 19:32:51 -0300

    Adicao de for in lista

--- Somando_novos_dicionarios.py
+++ Somando_novos_dicionarios.py
@@ -1,19 +1,21 @@
-lista = [ numero * 2 for numero in range(10)]
-print (lista)
+import pprint
 
-print()
+def p(v):
+    pprint.pprint (v, sort_dicts = False, width = 40)
 
 produtos = [
-    {'nome': 'p1', 'preco': 20,},
-    {'nome': 'p2', 'preco': 10,},
-    {'nome': 'p3', 'preco': 30,},
+    { 'nome': 'p1', 'preco': 20, },
+    { 'nome': 'p2', 'preco': 10, },
+    { 'nome': 'p3', 'preco': 30, },
 ]
 
 novos_produtos = [
     {**produto, 'preco': produto['preco'] * 1.05}
+    if produto ['preco'] > 10 else {**produto}
     for produto in produtos
 ]
 
-print(*novos_produtos, sep='\n')
+p(novos_produtos)
+
 
 print()
