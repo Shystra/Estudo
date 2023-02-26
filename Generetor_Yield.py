04b0459d6dbb3875b0f60f208132ae9eb13b0bb3
Shystra <jlsp.2019@hotmail.com>
2023-02-26 14:21:07 -0300

    Alterações

--- Generetor_Yield.py
+++ Generetor_Yield.py
@@ -16,42 +16,51 @@ for n in gen:
     p(n)'''
 
 def gen1():
-    print('COMECOU GEN1')
+
+    p ('COMECOU GEN1')
+
     yield 1
     yield 2
     yield 3
-    print('ACABOU GEN1')
+
+    p ('ACABOU GEN1')
 
 
 def gen3():
-    print('COMECOU GEN3')
+
+    p ('COMECOU GEN3')
+
     yield 10
     yield 20
     yield 30
-    print('ACABOU GEN3')
+    
+    p ('ACABOU GEN3')
 
 
-def gen2(gen=None):
-    print('COMECOU GEN2')
+def gen2 (gen=None):
+
+    p ('COMECOU GEN2')
+    
     if gen is not None:
         yield from gen
     yield 4
     yield 5
     yield 6
-    print('ACABOU GEN2')
+    
+    p ('ACABOU GEN2')
 
 
-g1 = gen2(gen1())
-g2 = gen2(gen3())
-g3 = gen2()
+g1 = gen2 (gen1())
+g2 = gen2 (gen3())
+g3 = gen2 ()
 for numero in g1:
-    print(numero)
-print()
+    p (numero)
+p ()
 for numero in g2:
-    print(numero)
-print()
+    p (numero)
+p ()
 for numero in g3:
-    print(numero)
-print()
+    p (numero)
+p ()
 
 
