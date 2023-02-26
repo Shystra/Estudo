9affb44995f38bc0ca5a4ea24dc96a5213da2760
Shystra <jlsp.2019@hotmail.com>
2023-02-26 14:54:31 -0300

    Adiando Funções

---
+++ Adiando_Executando.py
@@ -0,0 +1,21 @@
+def soma (x, y):
+    return x + y
+
+def multiplica (x, y):
+    return x * y
+
+def executa (funcao, x):
+    def interna (y):
+        return funcao (x, y)
+    return interna
+
+def executay (funcao, y):
+    def internay (x):
+        return funcao (x, y)
+    return internay
+
+soma1 = executa (soma, 5)
+soma2 = executay (multiplica, 10)
+
+print(soma1 (10))
+print(soma2 (10))
