94fd19e4e17d5e545dc7fd7e9e07091d263bea5b
Shystra <jlsp.2019@hotmail.com>
2023-03-05 16:46:45 -0300

    Abrindo Spotify

---
+++ Automatizando/Abrindo_Programas.py
@@ -0,0 +1,20 @@
+import pyautogui
+import time
+
+pyautogui.PAUSE = 2
+
+pyautogui.press ("win")
+pyautogui.write ("Spotify")
+# pyautogui.press ("backspace") # Apaga uma letra
+
+pyautogui.press ("enter")
+pyautogui.click (x = 96, y = 102)
+pyautogui.write ("The Weeknd")
+pyautogui.press ("enter")
+
+pyautogui.click (x = 672, y = 364)
+
+
+time.sleep(3)
+pyautogui.position()
+print(pyautogui.position())
