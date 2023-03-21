4cc314fa60b8d7c8b21b4c6378ee8b2b722cfdf3
Shystra <jlsp.2019@hotmail.com>
2023-03-21 12:30:37 -0300

    Controlid

---
+++ Open_Sites/ControlID.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+ROOT_FOLDER = Path(__file__).parent
+# Caminho para a pasta onde o chromedriver está
+CHROME_DRIVER_PATH = ROOT_FOLDER / 'drivers' / 'chromedriver'
+
+
+def make_chrome_browser(*options: str) -> webdriver.Chrome:
+    chrome_options = webdriver.ChromeOptions()
+
+    chrome_options.add_argument('--headless')
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)
+
+    chrome_service = Service(
+        executable_path=str(CHROME_DRIVER_PATH),
+    )
+
+    browser = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options
+    )
+
+    return browser
+
+if __name__ == '__main__':
+    TIME_TO_WAIT = 2
+
+    options = "--headless"
+    browser = make_chrome_browser(*options)
+
+    
+    browser.get('192.168.50.150')
+    if browser.get is True:
+        print('acessou')
