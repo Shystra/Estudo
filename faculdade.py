f8266f4b0ba102371ce3cd523685fc2d87d6c916
Shystra <jlsp.2019@hotmail.com>
2023-04-11 21:11:45 -0300

    Av.1

---
+++ faculdade.py
@@ -0,0 +1,36 @@
+from threading import Thread
+
+minha_lista = []
+
+def funcao ():
+    for i in range(100000):
+        minha_lista.append(1)
+    for i in range(100000):
+        minha_lista.pop()
+
+if __name__== '__main__':
+    tarefas = []
+    for indice in range(10):
+        tarefa = Thread(target=funcao)
+        tarefas.append(tarefa)
+        tarefa.start()
+
+    print(len(minha_lista))
+    for tarefa in tarefas:
+        tarefa.join()
+    print(len(minha_lista))
+
+    
+
+a = 0
+for i in range (30):
+    if a % 2 == 0:
+        a += 1
+        continue
+    else:
+        if a % 5 == 0:
+            break
+        else:
+            a += 3
+            print (a)
+            
